9cc4ea5e1b738ab16e6cdb4e3e600481f2d2607c
freddie valone <freddie.valone@gmail.com>
2018-11-19 00:51:40 -0500

    Add vuex store

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbvue/build/css/mdb.css';
-import App from './App.vue'
+import App from './App';
+import store from './store';
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
